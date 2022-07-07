e39e5b65aebf637cc45622c08d5a35e34df7e368
bthalpin <brian.t.halpin@gmail.com>
2022-07-06 21:49:41 -0400

    Update resume

--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -20,7 +20,7 @@ const Resume = () => {
             <div className="skillContainer">
                 <div className="skills">
                     <h2>Technical Skills</h2>
-                    <p className="skill"><span className="skillLevel">Front-End:</span> HTML, CSS, Bootstrap, JavaScript, TypeScript, jQuery, React, Redux</p>
+                    <p className="skill"><span className="skillLevel">Front-End:</span> HTML, CSS, Bootstrap, JavaScript, TypeScript, jQuery, React, React-Native, Redux</p>
                     <p className="skill"><span className="skillLevel">Back-End:</span> Node, Express, Handlebars, Django</p>
                     <p className="skill"><span className="skillLevel">Database:</span> MongoDB, Mongoose, GraphQL, Apollo, MySQL, Sequelize, Postgres, Knex </p>
                     <p className="skill"><span className="skillLevel">Other:</span> Python, JSON Web Tokens, bcrypt, socket.io, Heroku, Git, Bash, Insomnia, MongoDb Compass, Microsoft Office Suite</p>
