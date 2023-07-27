95a55c43527f6178adbad2613708a77b58c79602
bthalpin <brian.t.halpin@gmail.com>
2023-07-26 22:46:19 -0400

    Change some of the styles, remove hover effects and change nav for mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,30 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import Nav from './Nav';
 import styles from './header.module.css';
 import {navigationLinks} from './navigationData';
 
-const Header = () => {
-    const [open, setOpen] = useState(false)
+type Props = {
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const Header: React.FC<Props> = ({ open, setOpen }) => {
+
+    const handleClose = (e:React.MouseEvent<HTMLDivElement>) => {
+        
+        const targetEl = e.target as Element;
+        const target = targetEl.classList[0];
+        if (target.includes('navContainerOpen')) {
+            setOpen(false)
+        }
+    }
+
     return (
-        <div className={styles.navContainer}>
+        <div className={open ? styles.navContainerOpen : ''} onClick={(e)=> handleClose(e)}>
+            <div  className={styles.navContainer}>
+                <Link to='/' className={styles.navName}>
+                    
+                        HALPIN
 
-            <Link to='/' className={styles.navName}>
-                
-                    HALPIN
+                </Link>
+                <div className={open ? styles.navLinkOpen : styles.navLinkContainer}>  
 
-            </Link>
-            <div className={open ? styles.navLinkOpen : styles.navLinkContainer}>  
-
-                {/* Navigation Links mapped from the navagationData.js file */}
-                {navigationLinks.map((navLink,index)=>
-                    <div key={index}>
-                        <Nav navLink={navLink} setOpen={setOpen}/>
+                    {/* Navigation Links mapped from the navagationData.js file */}
+                    {navigationLinks.map((navLink,index)=>
+                        <div key={index}>
+                            <Nav navLink={navLink} setOpen={setOpen}/>
+                        </div>
+                    )}
+                </div>
+                <div className={open ? styles.menu_open : styles.menu_container} onClick={()=>setOpen(!open)}>
+                    <div className={styles.menu} >
                     </div>
-                )}
-            </div>
-            <div className={open ? styles.menu_open : styles.menu_container} onClick={()=>setOpen(!open)}>
-                <div className={styles.menu} >
                 </div>
             </div>
         </div>
